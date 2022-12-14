0a7196ed99546dbb928c9b2d09a96a52b038b53a
ntaulya <netaaulya@gmail.com>
2022-12-15 00:00:38 +0700

    pulsa page

--- src/pages/dashboard/products/telecommunication/pulsa/AddProductsTelkomsel.jsx
+++ src/pages/dashboard/products/telecommunication/pulsa/AddProductsTelkomsel.jsx
@@ -2,40 +2,124 @@ import SidebarPage from "../../../../../components/dashboard/sidebar/Sidebar";
 import { Switch } from "antd";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import moment from 'moment';
 
 const AddProductsTelkomsel = () => {
   const navigate = useNavigate();
-  const [toggle, setToggle] = useState(false);
   const [showPromo, setShowPromo] = useState(false);
   const [hargaStok, setHargaStok] = useState(0);
-
-  const handleToggle = () => {
-    let data = toggle;
-
-    if (data === false) setToggle(true);
-    else {
-      setToggle(false);
-    }
-  };
+  const [startDate, setStartDate] = useState(new Date());
+  const [finishDate, setFinishDate] = useState(new Date());
 
   const backToTelkomsel = () => {
     navigate(-1);
   };
 
-  const _renderNumeric = () => {
-    let number = Number(hargaStok);
-    return number?.toLocaleString("id-ID", {
-      style: "currency",
-      currency: "IDR",
-      maximumFractionDigits: 0,
-      minimumFractionDigits: 0,
-    });
+  const initialValues = {
+    namaproduk: "",
+    stok: "",
+    harga: "",
+    status: "",
+    hargapromo: "",
+    promoperiodeawal: "",
+    promoperiodeakhir: "",
   };
 
-  const handleSubmit = (e) => {
-    let data = e.target.value.replace(/\d{1,3}/g);
-    setHargaStok(`${_renderNumeric(data)}`);
+  const [formValues, setFormValues] = useState(initialValues);
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [Valuedate, setValuedate] = useState("");
+  console.log("test", Valuedate)
+
+  console.log("masuk", formValues);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
   };
+
+  const handleChangeStartDate = (e) => {
+    const startDate = e.target.value;
+    setFormValues((prev) => ({ ...prev, promoperiodeawal: startDate }));
+  };
+
+  const handleChangeFinishDate = (e) => {
+    const finishDate = e.target.value;
+    setFormValues((prev) => ({ ...prev, promoperiodeakhir: finishDate }));
+  };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const errors = validate(formValues);
+  //   setIsSubmit(true);
+
+  //   const datauser = new FormData();
+  //   datauser.append("name", formValues.namaproduk);
+  //   datauser.append("stock", formValues.stok);
+  //   datauser.append("price", formValues.harga);
+  //   datauser.append("status", formValues.status);
+  //   datauser.append("discount", formValues.hargapromo);
+  //   datauser.append("promo_start_date", formValues.promoperiodeawal);
+  //   datauser.append("promo_end_date", formValues.promoperiodeakhir);
+
+  //   if (Object.keys(errors).length === 0 && isSubmit) {
+  //     await AxiosInstance.post("/admin/users", datauser, {
+  //       headers: {
+  //         Authorization: "Bearer " + token,
+  //       },
+  //     })
+  //       .then(() => {
+  //         setLoading(true);
+  //         onClose(true);
+  //         toast.success("Data Produk Berhasil Ditambahkan");
+  //       })
+  //       .catch((err) => {
+  //         toast.error("Data Produk Gagal Ditambahkan");
+  //       });
+  //   } else {
+  //     Object.values(errors).map((err) => alert(err));
+  //   }
+  // };
+
+  // const validate = (values) => {
+  //   const errors = {};
+  //   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+  //   // DataUsers.map((user) => {
+  //   //   if (values.name === user.name) {
+  //   //     errors.name = "Username has been ";
+  //   //   }
+  //   //   if (values.email === user.email) {
+  //   //     errors.email = "email has been ";
+  //   //   }
+  //   //   if (values.phone_number === user.phone_number) {
+  //   //     errors.phone_number = "Handphone has been ";
+  //   //   }
+  //   // });
+  //   if (values.produk === null || values.produk === "") {
+  //     errors.produk = "Produk harus diisi";
+  //   }
+
+    // if (!values.email) {
+    //   errors.email = "Email is required!";
+    // } else if (!regex.test(values.email)) {
+    //   errors.email = "This is not a valid email format!";
+    // }
+
+    // if (!values.phone_number) {
+    //   errors.phone_number = "phone_number is required!";
+    // }
+
+    // if (!values.password) {
+    //   errors.password = "Password is required";
+    // } else if (values.password.length < 4) {
+    //   errors.password = "Password must be more than 4 characters";
+    // }
+    // else if (values.password.length > 10) {
+    //   errors.password = "Password cannot exceed more than 10 characters";
+    // }
+
+    // return errors;
+  // };
 
   return (
     <>
@@ -50,26 +134,23 @@ const AddProductsTelkomsel = () => {
                 <label>Nama Produk</label>
                 <input
                   className="border-solid border border-black w-[328px] h-14 rounded-xl px-2 "
-                  name="produk"
+                  name="namaproduk"
                   type="text"
+                  value={formValues.namaproduk}
                   placeholder="Pulsa Rp. 5.000,00"
+                  onChange={handleChange}
                 />
               </div>
               <div className="flex justify-between items-center mb-4">
                 <label>Stok</label>
                 <input
                   className="border-solid border border-black w-[328px] h-14 rounded-xl px-2"
-                  name="produk"
+                  name="stok"
                   type="text"
+                  value={formValues.stok}
                   placeholder="Jumlah Stok..."
+                  onChange={handleChange}
                 />
-              </div>
-              <div className="flex justify-between items-center mb-4">
-                <label>Status Produk</label>
-                <div className="w-[328px] flex">
-                  <Switch onChange={handleToggle} className=" bg-grey2 mr-2" />
-                  {toggle ? <div>Aktif</div> : <div>Non Aktif</div>}
-                </div>
               </div>
 
               {/* Harga*/}
@@ -77,13 +158,11 @@ const AddProductsTelkomsel = () => {
               <div className="flex justify-between items-center mb-4">
                 <label>Harga Produk</label>
                 <input
-                  value={hargaStok}
-                  onChange={(e) => {
-                    handleSubmit(e);
-                  }}
+                  onChange={handleChange}
                   className="border-solid border border-black w-[328px] h-14 rounded-xl px-2"
-                  name="produk"
+                  name="harga"
                   type="text"
+                  value={formValues.harga}
                   placeholder="Pulsa Rp. 5.000,00"
                 />
               </div>
@@ -94,18 +173,20 @@ const AddProductsTelkomsel = () => {
                 <div className="flex justify-start items-center w-[328px]">
                   <input
                     className="border-solid border border-black w-5 h-5 rounded-xl px-2 mr-4"
-                    name="produk"
+                    name="status"
                     type="radio"
-                    value="normal"
+                    value="false"
                     onClick={() => setShowPromo(false)}
+                    onChange={handleChange}
                   />
                   <label className="pr-10">Normal</label>
                   <input
                     className="border-solid border border-black w-5 h-5 rounded-xl px-2 mr-4"
-                    name="produk"
+                    name="status"
                     type="radio"
-                    value="promo"
+                    value="true"
                     onClick={() => setShowPromo(true)}
+                    onChange={handleChange}
                   />
                   <label>Promo</label>
                 </div>
@@ -116,9 +197,11 @@ const AddProductsTelkomsel = () => {
                     <label>Harga Promo</label>
                     <input
                       className="border-solid border border-black w-[328px] h-14 rounded-xl px-2"
-                      name="produk"
+                      name="hargapromo"
                       type="text"
+                      value={formValues.hargapromo}
                       placeholder="Pulsa Rp. 5.000,00"
+                      onChange={handleChange}
                     />
                   </div>
                   <div className="flex justify-between items-start mb-4">
@@ -126,16 +209,20 @@ const AddProductsTelkomsel = () => {
                     <div className="flex flex-col justify-end">
                       <input
                         className="border-solid border border-black w-[328px] h-14 rounded-xl px-2"
-                        name="produk"
+                        name="periodepromoawal"
                         type="date"
+                        value={formValues.promoperiodeawal}
                         placeholder="Pulsa Rp. 5.000,00"
+                        onChange={handleChangeStartDate}
                       />
                       <label className="mb-3">DD/MM/YYYY</label>
                       <input
                         className=" border-solid border border-black w-[328px] h-14 rounded-xl px-2 "
-                        name="produk"
+                        name="periodepromoakhir"
                         type="date"
+                        value={formValues.periodepromoakhir}
                         placeholder="Pulsa Rp. 5.000,00"
+                        onChange={handleChangeFinishDate}
                       />
                       <label>DD/MM/YYYY</label>
                     </div>
